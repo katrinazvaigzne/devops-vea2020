7d89f62f7efdf274a9e3f218fb7142c838d964ce
Bils Gutfricis <b.gutfricis@gmail.com>
2020-11-03 22:45:41 +0200

    Initial commit

---
+++ src/main/java/hello/Main.java
@@ -0,0 +1,8 @@
+package hello;
+
+public class Main {
+    public static void main(String[] args) {
+        Greeter greeter = new Greeter();
+        System.out.println(greeter.sayHello());
+    }
+}
